94b795041ae5417bf770cb920e082a080b772227
JuanDAT-3 <aguilart.juan@outlook.com>
2024-07-10 20:46:34 -0500

    Arreglos Finales.

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px 
 
-vhc= pd.read_csv(r'C:\Users\Usuario\Desktop\Trabajo_aprendizaje\TripleTen\Sprint-5-Project\vehicles_us.csv')
+vhc= pd.read_csv(r'C:\Users\Usuario\Desktop\Trabajo_aprendizaje\TripleTen\Sprint-5-Project\vehicles_us_cleaned.csv')
 
 #Despliegue de app
 
 st.header('Analisis Exploratorio de datos')
 
-st.write(f'Datos presentes. {vhc.head(10)}')
+st.write('Visualización de datos de vehiculos en estados unidos basados en su kilometraje.')
 
-hist_button= st.button('Histograma')
+hist_button= st.checkbox('Histograma')
+
+dis_button= st.checkbox('Diagrama de Dispersión')
 
 if hist_button:
 
-    fig= px.histogram(vhc)
+    fig= px.histogram(vhc, x='odometer',range_x=[0,500000], nbins=150)
 
-st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
+
+if dis_button:
+    fig = px.scatter(vhc, x="odometer", y="price",range_x=[0,600000]) # crear un gráfico de dispersión
+
+    st.plotly_chart(fig, use_container_width=True)
